90c420db2005a8d222236faa6fd07c1f74a0a077
Unknowd <svenholtrop@gmail.com>
2016-11-28 21:13:24 +0100

    De basis
    Een level opslag
    Basis voor een tegel
    Basis voor een wezen

--- Main.py
+++ Main.py
@@ -1,1 +1,25 @@
+class Level:
+    def __init__(self, level):
+        self.level = level
+        
+    def getLevel(self, objecten):
+        level = copy.deepcopy(self.level)
+        for i in objecten:
+            level[i.y][i.x] = i
+        return level
 
+class Tile:
+    def __init__(self, shape):
+        self.shape = shape
+
+    def __repr__(self):
+        return self.shape
+
+class Entity:
+    def __init__(self, x, y, shape):
+        self.x = x
+        self.y = y
+        self.shape = shape
+
+    def __repr__(self):
+        return self.shape
