b696deba83ada5c0faaf4325ee32c7928b3829c6
Unknowd <svenholtrop@gmail.com>
2016-11-28 21:36:01 +0100

    Level import functionaliteit
    Level kan worden ingeport

--- Main.py
+++ Main.py
@@ -1,13 +1,27 @@
 class Level:
-    def __init__(self, level):
-        self.level = level
+    def __init__(self):
+        self.level = []
+
+    def addLine(self, line):
+        self.level.append([Tile(i) for i in line])
         
     def getLevel(self, objecten):
         level = copy.deepcopy(self.level)
         for i in objecten:
             level[i.y][i.x] = i
         return level
+    
+    def isEmpty(self):
+        return self.level
 
+    def __repr__(self):
+        s = ''
+        for i in self.level:
+            for j in i:
+                s += str(j)
+            s += '\n'
+        return s
+    
 class Tile:
     def __init__(self, shape):
         self.shape = shape
@@ -23,3 +37,17 @@ class Entity:
 
     def __repr__(self):
         return self.shape
+class Game:
+    def __init__(self, file):
+        self.levels = []
+        with open(file, 'r') as f:
+            current = Level()
+            for i in f:
+                if i == '\n':
+                    self.levels.append(current)
+                    current = Level()
+                    continue
+                current.addLine(i)
+                
+        print(self.levels[0])
+                
