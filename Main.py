66df51dba318bc6a8234d34897948b506ea80245
Pim <pim-is@live.nl>
2016-11-29 18:11:22 +0100

    Tile-systeem verbeterd.
    Tile-systeem zodanig veranderd dat niet elk teken zijn eigen Tile
    krijgt, maar elke tekensoort.

--- Main.py
+++ Main.py
@@ -1,5 +1,29 @@
 from Structures import *
 
+def metadata(invoer):
+    eigenschappen = {}
+    geopend = False
+    dubbelepunt = False
+    sleutel = ''
+    waarde = ''
+    for i in invoer:
+        if i == '[':
+            geopend = True
+        elif i == ']':
+            geopend = False
+            dubbelepunt = False
+            eigenschappen[sleutel] = waarde
+            sleutel = ''
+            waarde = ''
+        elif i == ':':
+            dubbelepunt = True
+        elif geopend == True and dubbelepunt == False:
+            sleutel += i
+        elif geopend == True and dubbelepunt == True:
+            waarde += i
+            
+    return eigenschappen
+
 class Game:
     def __init__(self, file):
         self.importLevels(file)
@@ -7,14 +31,26 @@ class Game:
     def importLevels(self, file):
         self.levels = []
         with open(file, 'r') as f:
-                current = Level()
-                for i in f:
+            current = Level()
+            begin = True
+            nieuw = True
+            for i in f:
+                if i == '\n':
+                    begin = False
+                elif begin:
+                    current.addTile(metadata(i))
+                else:
                     if i == '\n':
                         self.levels.append(current)
                         current = Level()
+                        nieuw = True
+                        continue
+                    if nieuw == True:
+                        # !laten staan: voor later gebruik!
+                        nieuw = False
                         continue
                     current.addLine(i)
                 self.levels.append(current)
                 
-game = Game('Test-Level.txt')
+game = Game('Test-level.txt')
                 
